c43bef22be8ec0c4a065ae038fa9fa81f6c98c73
acdefgl <damilycruz@gmail.com>
2017-05-02 13:41:51 -0400

    windowsapplication1

---
+++ WindowsApplication2/WindowsApplication2/Module1.vb
@@ -0,0 +1,20 @@
+﻿
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+
+Module Biblioteca
+    Public conexion As New MySqlConnection
+
+    Public Sub ConectarSql()
+        Try
+            conexion.ConnectionString = "server=localhost;user id=root; password=''; database = dbbiblioteca"
+            conexion.Open()
+            MessageBox.Show("conectado")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+End Module
+
